b48409745ef750a149b6d7958a159c50338eb7b2
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-11-02 12:56:40 -0300

    ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,5 @@ import {sleep} from 'k6';
 //Base de execução dos testes
 export default function(){ 
     http.get('https://test.k6.io'); //executar uma função via http para o endereço especificado
-    sleep(1); //
+    sleep(1); //segundos em sleep
 }
